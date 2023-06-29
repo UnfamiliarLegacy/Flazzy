f407c4450ef0740cac297c29b2d261bcc5b77a1f
UnfamiliarLegacy <74633542+UnfamiliarLegacy@users.noreply.github.com>
2023-06-29 05:59:23 +0200

    Correct ZLib behaviour
    See https://github.com/dotnet/runtime/issues/62858

--- Flazzy/Compression/ZLIB.cs
+++ Flazzy/Compression/ZLIB.cs
@@ -14,25 +14,35 @@ public static class ZLIB
             return output.ToArray();
         }
     }
-    
+
     public static FlashReader WrapDecompressor(Stream input, bool leaveOpen = false)
     {
         return new FlashReader(new ZLibStream(input, CompressionMode.Decompress, leaveOpen));
     }
-    
+
     public static FlashWriter WrapCompressor(Stream output, bool leaveOpen = false)
     {
         return new FlashWriter(new ZLibStream(output, CompressionLevel.SmallestSize, leaveOpen));
     }
-        
-    public static unsafe void DecompressFast(ReadOnlySpan<byte> source, byte[] destination, int decompressedSize) {
-        fixed (byte* pBuffer = &source[0]) {
-            using (var stream = new UnmanagedMemoryStream(pBuffer, source.Length)) 
-            using (var deflateStream = new ZLibStream(stream, CompressionMode.Decompress)) {
-                var read = deflateStream.Read(destination, 0, decompressedSize);
-                if (read != decompressedSize)
+
+    public static unsafe void DecompressFast(ReadOnlySpan<byte> source, Span<byte> destination, int decompressedSize)
+    {
+        fixed (byte* pBuffer = &source[0])
+        {
+            using (var stream = new UnmanagedMemoryStream(pBuffer, source.Length))
+            using (var deflateStream = new ZLibStream(stream, CompressionMode.Decompress))
+            {
+                var readTotal = 0;
+
+                while (deflateStream.Read(destination) is int read and > 0)
                 {
-                    throw new Exception($"Decompressed incorrect amount of bytes ({read} != {decompressedSize})");
+                    readTotal += read;
+                    destination = destination.Slice(read);
+                }
+
+                if (readTotal != decompressedSize)
+                {
+                    throw new Exception($"Decompressed incorrect amount of bytes ({readTotal} != {decompressedSize})");
                 }
             }
         }
